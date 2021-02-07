63edbd26b8ff8b306e37339fbc23df05ba62a678
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2021-02-07 22:40:32 +0700

    fix

--- x.js
+++ x.js
@@ -1,35 +1,41 @@
+
 const cafeStore = {};
 
 var prices = 0;
 
 const Price = {
-  Cafe: 12000,
-  CafeMilk: 18000,
-  dame: 15000,
-  Dua: 15000,
-  Duatay: 25000,
-  lemon: 10000,
-  lipton: 12000,
-  orange: 15000,
-  orangemilk: 20000,
-  rauma: 10000,
-  raumadua: 15000,
-  raumasua: 15000,
-  tea: 10000,
+  Cafe: 17000,
+  CafeMilk: 22000,
+  tea: 12000,
+  lipton: 17000,
+  lemon: 12000,
+  rauma: 20000,
+  raumasua: 25000,
+  raumadua: 25000,
+  Dua: 25000,
+  Duatay: 30000,
+  orange: 20000,
+  orangemilk: 25000,
+  dame: 20000,
+  nuocngot: 20000,
+  nuocsuoi: 12000,
+  taythem: 5000,
+  mytrung: 20000,
+  oplacamoi: 35000,
+  baso: 35000,
+  conmeo: 25000
 };
 
 function getInfo() {
-  const inputArray = document.querySelectorAll('input');
-      for (const item of inputArray) {
-        var temp = 0;
-        cafeStore[item.name] = item.value;
-        if(Price[item.name] != 0){
-        temp = parseInt(item.value) * parseInt(Price[item.name]);
-        prices = prices + temp;
-        }
-      }
-  console.log(prices);
+  const inputArray = document.querySelectorAll("input");
+  for (const item of inputArray) {
+    var temp = 0;
+    cafeStore[item.name] = item.value;
+    if (Price[item.name] != 0) {
+      temp = parseInt(item.value) * parseInt(Price[item.name]);
+      prices = prices + temp;
+    }
+  }
   document.getElementById("Total").innerHTML = prices;
   prices = 0;
 }
-
